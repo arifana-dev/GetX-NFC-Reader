29e4f615836de84534aad878969a61c9c0f5a4fd
Arifana <arifana004@gmail.com>
2023-02-04 23:55:15 +0700

    Update home UI

---
+++ lib/application/constants/colors.dart
@@ -0,0 +1,6 @@
+import 'package:ar_nfc_reader/application/helpers/hex_color.dart';
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static Color background = HexColor.fromHex('#f6f8fe');
+}
